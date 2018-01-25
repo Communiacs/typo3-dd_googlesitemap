d8306215e550cc365dda0cb1ef4c66e9eb58beac
Denis Mir <dm@communiacs.de>
2018-01-25 14:10:50 +0100

    [TASK] Prepares version 2.1.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array (
   'clearCacheOnLoad' => 0,
   'lockType' => '',
   'author_company' => 'SIA "ACCIO"',
-  'version' => '2.1.6',
+  'version' => '2.1.7',
   'constraints' => 
   array (
     'depends' => 
