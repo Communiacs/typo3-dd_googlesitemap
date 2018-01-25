fda0e046d7aa9b0735197ffc02b8b3f45fd03bb9
Denis Mir <dm@communiacs.de>
2018-01-25 13:58:20 +0100

    [TASK] Prepare release 2.1.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array (
   'clearCacheOnLoad' => 0,
   'lockType' => '',
   'author_company' => 'SIA "ACCIO"',
-  'version' => '2.1.4',
+  'version' => '2.1.6',
   'constraints' => 
   array (
     'depends' => 
